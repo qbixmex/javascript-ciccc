7419eea555470393f23fa72807e986fa55a9faf7
Daniel González Briseño <qbixmex@gmail.com>
2023-05-16 14:03:56 -0700

    Assigning Variables

--- basics.js
+++ basics.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+//* Declare and assign variables
+let userName = "qbixmex";
+let firstName = "Daniel";
+let lastName = "González Briseño";
+
+console.log("User Name:", userName);
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Full Name:", firstName, lastName);
+
+//* Declare variables
+let age;
+age = 10;
+console.log("Age:", age);
