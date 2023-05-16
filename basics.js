fe7c7d68c3fe54f93d59ac090af6803104e7ab43
Daniel González Briseño <qbixmex@gmail.com>
2023-05-16 14:31:01 -0700

    Constants

--- basics.js
+++ basics.js
@@ -12,3 +12,22 @@ console.log("Full Name:", firstName, lastName);
 let age;
 age = 10;
 console.log("Age:", age);
+
+//! You can't re-declare a variable
+//! let age;
+
+age = 12;
+
+console.log("Age:", age);
+
+//* CONSTANTS
+const planet = "EARTH";
+
+console.log("Planet:", planet);
+
+//! You can't assign a constant variable
+//! planet = "MARS";
+
+//! You can't print constant 
+//! const year;
+//! console.log(year);
