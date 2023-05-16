fd7e309bfd2586d69bab96cbc204f32146e65cb5
Daniel González Briseño <qbixmex@gmail.com>
2023-05-16 15:45:23 -0700

    Functions Definitions

--- basics.js
+++ basics.js
@@ -1,25 +1,7 @@
-var number = 5;
-
-/**
- * Prints a number and add 2 to that one.
- */
-function addTwo() {
-
-  console.log("Running addTwo")
-
-  const additionBy = 2;
-  let sum;
-
-  if ( number < 10 ) { //* TRUE
-    sum = number + additionBy;
-    console.log(sum);
-  } else { //* FALSE
-    sum = number;
-    console.log(sum);
-  }
-
-  console.log("Final Sum:", sum);
-
+//* Function Definition
+function nameOfTheFunction() {
+  //* Function Logic
 }
 
-addTwo();
+//* Calling function
+nameOfTheFunction();
