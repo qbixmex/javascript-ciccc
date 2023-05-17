ec2d98007fe1aa3a0b889273099b28ffd7732942
Daniel González Briseño <qbixmex@gmail.com>
2023-05-17 15:51:01 -0700

    For Loops

---
+++ for.js
@@ -0,0 +1,13 @@
+console.log("\nFor Loops\n");
+
+for (let count = 0; count <= 5; count++) {
+  console.log(count);
+}
+
+console.log("\nNested Loops\n");
+
+for (let x = 0; x <= 3; x++) {
+  for (let y = 0; y <= 3; y++) {
+    console.log(x + "," + y);
+  }
+}
