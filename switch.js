a4e6c019ee9eba4c107c75f6ac72a18eefa2950d
Daniel González Briseño <qbixmex@gmail.com>
2023-05-17 15:36:53 -0700

    Switch

---
+++ switch.js
@@ -0,0 +1,31 @@
+let fruit = 'apple';
+
+switch(fruit) {
+  case 'apple':
+    console.log(0.25);
+    break;
+  case 'banana':
+    console.log(0.15);
+    break;
+  case 'pear':
+    console.log(0.35);
+    break;
+  default:
+    console.log(0);
+}
+
+let number = 2;
+
+switch(number) {
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+    console.log(2);
+    break;
+  case 3:
+    console.log(3);
+    break;
+  default:
+    console.log(0);
+}
