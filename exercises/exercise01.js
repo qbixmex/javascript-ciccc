7ea0f42343a20488db0fa4f0f457b51ca28c7e01
Daniel González Briseño <qbixmex@gmail.com>
2023-05-18 15:17:20 -0700

    Exercise 1

---
+++ exercises/exercise01.js
@@ -0,0 +1,13 @@
+/*
+  Write an if..else statement that:
+    - Prints "even" if the number is an even number
+    - Prints "odd" if the number is an odd number
+
+  Hint: Use the %(module) operator to determine is a number is even or odd.
+*/
+
+console.log();
+
+const number = 8;
+
+console.log((number % 2 == 0) ? "even" : "odd");
