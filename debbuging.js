9d31766fa214139227a030d1ac51d84c3b9079c2
Daniel González Briseño <qbixmex@gmail.com>
2023-05-24 15:57:11 -0700

    Visual Studio Debugger

---
+++ debbuging.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 11;
+let c = 12;
+let d = 13;
+let e = 14;
+
+let sum = a + b + c;
+
+function addTwoNumbers(a, b) {
+  let sum = a + b;
+  let message = `Sum of ${a} and ${b} is ${sum}`;
+  console.log(message);
+}
+
+addTwoNumbers(a, b);
